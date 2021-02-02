c2662b9df0daf4ef7cb891b752f133ae3fb3b7e0
rbermudez967 <77324380+rbermudez967@users.noreply.github.com>
2021-02-02 10:25:36 -1000

    Completed parts 1 - 4, need to complete pt 5

---
+++ SP2.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//SP2 Introduction to If/Else Statements (Part 1)
+//Class 7 Feb 2
+
+// So, if/else statements allow our programs to make decisions badsed on whether the conditional statement evaluates to true OR false. Based on what the conditional evaluates to, code executes in the if code block or if false, code executes in the else block.
+
+//Part 2
+
+var goodGrade = "I've been doing all my work"
+
+if goodGrade == "I'm not doing very well"
+{
+//if block
+    print ("I should be getting an A at this point")
+    
+}
+
+//Part 3
+else
+{
+    
+    //else block
+    print ("I need to work harder")
+}
+
+//Part 4
+
+var gradeA = true
+
+//create an if/else statement on your own
+
+if gradeA == false
+{
+print ("I really like this class")
+}
+
+else
+{
+    print ("I'm not so sure about coding in swift")
+}
+
